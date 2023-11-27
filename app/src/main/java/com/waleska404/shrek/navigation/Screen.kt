291525a3a9e33e1e41e2d4fd8484cce33a88e378
Paula Boyano Ivars <paula.boyano@vass.es>
2023-11-27 10:50:29 +0000

    :tada: Initial commit

---
+++ app/src/main/java/com/waleska404/shrek/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package com.waleska404.shrek.navigation
+
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash_screen")
+    object Welcome : Screen("welcome_screen")
+    object Home : Screen("home_screen")
+    object Details : Screen("details_screen/{characterId}") {
+        fun passCharacterId(id: String) = "details_screen/$id"
+    }
+    object Search : Screen("search_screen")
+}
