48555c8988426d2e024282e65ff3c3b3b1c09acf
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-30 19:29:28 +0100

    :construction: network provider setup

---
+++ app/src/main/java/com/waleska404/shrek/di/NetworkModule.kt
@@ -0,0 +1,63 @@
+package com.waleska404.shrek.di
+
+import androidx.paging.ExperimentalPagingApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.waleska404.shrek.data.remote.ShrekApi
+import com.waleska404.shrek.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@ExperimentalPagingApi
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShrekApi(retrofit: Retrofit): ShrekApi {
+        return retrofit.create(ShrekApi::class.java)
+    }
+
+    /*
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(
+        shrekApi: ShrekApi,
+        shrekDatabase: ShrekDatabase
+    ): RemoteDataSource {
+        return RemoteDataSourceImpl(
+            borutoApi = borutoApi,
+            borutoDatabase = borutoDatabase
+        )
+    }*/
+}
