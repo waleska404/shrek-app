aeb652a53c7fac1b45fa17b98ee272c127f0fc04
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-07 17:28:05 +0100

    :construction: database for testing

--- app/src/main/java/com/waleska404/shrek/data/local/ShrekDatabase.kt
+++ app/src/main/java/com/waleska404/shrek/data/local/ShrekDatabase.kt
@@ -1,6 +1,8 @@
 package com.waleska404.shrek.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.waleska404.shrek.data.local.dao.CharacterDao
@@ -11,6 +13,19 @@ import com.waleska404.shrek.domain.model.ShrekCharacter
 @Database(entities = [ShrekCharacter::class, CharacterRemoteKeys::class], version = 1, exportSchema = false)
 @TypeConverters(DatabaseConverter::class)
 abstract class ShrekDatabase: RoomDatabase() {
+
+    companion object {
+        fun create(context: Context, useInMemory: Boolean): ShrekDatabase {
+            val databaseBuilder = if (useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, ShrekDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, ShrekDatabase::class.java, "test_database.db")
+            }
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
 
     abstract fun characterDao(): CharacterDao
     abstract fun characterRemoteKeysDao(): CharacterRemoteKeysDao
